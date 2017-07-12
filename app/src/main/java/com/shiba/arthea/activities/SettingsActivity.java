e50997f231333dc8dd1c7f8e9c61f454a3d78e7c
Alex Badea <Alex-Badea@github.com>
2017-07-12 18:54:29 +0300

    added gitignore, settings activity

---
+++ app/src/main/java/com/shiba/arthea/activities/SettingsActivity.java
@@ -0,0 +1,18 @@
+package com.shiba.arthea.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.shiba.arthea.R;
+
+public class SettingsActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+    }
+}
